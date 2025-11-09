4ad1d15cdd7e0655b36f53f6d65be468462ac34e
An6not <leonidleon582@gmail.com>
2025-11-09 02:11:52 +0200

    Add click event listener to app icons

--- Public/script.js
+++ Public/script.js
@@ -1,1 +1,10 @@
 
+const appIcons = document.querySelectorAll('.app-icon');
+
+appIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const appName = icon.dataset.app;
+        alert(`Открытие приложения: ${appName}`);
+        // Здесь позже можно добавить открытие окна приложения
+    });
+});
